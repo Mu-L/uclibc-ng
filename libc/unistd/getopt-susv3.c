Mu-L/uclibc-ng

commit 50aba30aee11d4fa51948c6ba258ec0bbdbae8c0

remove libintl stub and libintl.h header
As __UCLIBC_HAS_GETTEXT_AWARENESS__ is never defined, this is
mostly dead code. It is planned to integrate libiconv-tiny
and gettext-tiny into uClibc-ng after the next release, so
that more software packages can be used without modification.

Remove any _/_N macro usage.

libc/unistd/getopt-susv3.c
@@ -22,7 +22,6 @@
 #include <string.h>
 #include <stdio.h>
 #include <getopt.h>
-#include <libintl.h>
 
 #ifdef __BCC__
 static const char missing[] = "option requires an argument";

Makefile.in
@@ -32,7 +32,6 @@ endif # ifeq ($(HAVE_DOT_CONFIG),y)
 include $(top_srcdir)extra/locale/Makefile.in
 include $(top_srcdir)ldso/Makefile.in
 include $(top_srcdir)libcrypt/Makefile.in
-include $(top_srcdir)libintl/Makefile.in
 include $(top_srcdir)libm/Makefile.in
 include $(top_srcdir)libutil/Makefile.in
 include $(top_srcdir)libpthread/Makefile.in
@@ -273,7 +272,6 @@ HEADERS_RM-$(UCLIBC_HAS_FLOATS)              += complex.h fpu_control.h ieee754.
 	bits/math*.h
 HEADERS_RM-$(findstring y,$(UCLIBC_HAS_FTW)$(UCLIBC_HAS_NFTW))  += ftw.h
 HEADERS_RM-$(UCLIBC_HAS_FTS)                 += fts.h
-HEADERS_RM-$(UCLIBC_HAS_GETTEXT_AWARENESS)   += libintl.h
 HEADERS_RM-$(UCLIBC_HAS_GLIBC_CUSTOM_PRINTF) += printf.h
 HEADERS_RM-$(UCLIBC_HAS_GLOB)                += glob.h
 HEADERS_RM-$(UCLIBC_HAS_GNU_ERROR)           += error.h
@@ -376,9 +374,6 @@ EMPTY_LIB_NAMES += crypt
 endif
 ifeq ($(UCLIBC_HAS_LIBUTIL),y)
 EMPTY_LIB_NAMES += util
-endif
-ifeq ($(UCLIBC_HAS_GETTEXT_AWARENESS),y)
-EMPTY_LIB_NAMES += intl
 endif
 EMPTY_LIBS = $(EMPTY_LIB_NAMES:%=lib/lib%.a)
 

Makerules
@@ -39,7 +39,6 @@ $(eval $(call add_IS_IN_lib,rtld,$(ldso-y)))
 $(eval $(call add_IS_IN_lib,libc,$(libc-y) $(libc-static-y) $(libc-y:.o=.os) $(libc-shared-y) $(libc-nonshared-y)))
 $(eval $(call add_IS_IN_lib,libcrypt,$(libcrypt-a-y) $(libcrypt-so-y)))
 $(eval $(call add_IS_IN_lib,libdl,$(libdl-a-y) $(libdl-so-y)))
-$(eval $(call add_IS_IN_lib,libintl,$(libintl-a-y) $(libintl-so-y)))
 $(eval $(call add_IS_IN_lib,libm,$(libm-a-y) $(libm-so-y)))
 $(eval $(call add_IS_IN_lib,libpthread/$(PTNAME),$(libpthread-a-y) $(libpthread-so-y) $(libpthread-nonshared-y)))
 $(eval $(call add_IS_IN_lib,libpthread/$(PTNAME)_db,$(libthread_db-a-y) $(libthread_db-so-y)))
@@ -49,16 +48,12 @@ $(eval $(call add_IS_IN_lib,libubacktrace,$(libubacktrace-a-y) $(libubacktrace-s
 $(eval $(call add_IS_IN_lib,libuargp,$(libuargp-a-y) $(libuargp-so-y)))
 
 shared_objs = $(libc-y:.o=.os) $(libc-shared-y) $(libc-nonshared-y) \
-	$(libcrypt-so-y) $(libdl-so-y) \
-	$(libintl-so-y) $(libm-so-y) \
+	$(libcrypt-so-y) $(libdl-so-y) $(libm-so-y) \
 	$(libpthread-so-y) $(libpthread-nonshared-y) $(libthread_db-so-y) \
-	$(librt-so-y) \
-	$(ldso-y) \
-	$(libutil-so-y) $(libubacktrace-so-y) $(libuargp-so-y)
+	$(librt-so-y) $(ldso-y) $(libutil-so-y) $(libubacktrace-so-y) $(libuargp-so-y)
 
 ar_objs =  $(libc-y) $(libc-static-y) $(libcrypt-a-y) \
-	$(libdl-a-y) $(libintl-a-y) $(libm-a-y) \
-	$(libpthread-a-y) $(libthread_db-a-y) \
+	$(libdl-a-y) $(libm-a-y) $(libpthread-a-y) $(libthread_db-a-y) \
 	$(librt-a-y) $(libutil-a-y) $(libubacktrace-a-y) $(libuargp-a-y)
 ifeq ($(DOPIC),y)
 ar_objs := $(ar_objs:.o=.os)

include/libc-symbols.h
@@ -582,24 +582,6 @@ FIXME! - ?
 # define libdl_hidden_data_ver(local, name)
 #endif
 
-#if defined NOT_IN_libc && defined IS_IN_libintl
-# define libintl_hidden_proto(name, attrs...) hidden_proto (name, ##attrs)
-# define libintl_hidden_def(name) hidden_def (name)
-# define libintl_hidden_weak(name) hidden_weak (name)
-# define libintl_hidden_ver(local, name) hidden_ver (local, name)
-# define libintl_hidden_data_def(name) hidden_data_def (name)
-# define libintl_hidden_data_weak(name) hidden_data_weak (name)
-# define libintl_hidden_data_ver(local, name) hidden_data_ver(local, name)
-#else
-# define libintl_hidden_proto(name, attrs...)
-# define libintl_hidden_def(name)
-# define libintl_hidden_weak(name)
-# define libintl_hidden_ver(local, name)
-# define libintl_hidden_data_def(name)
-# define libintl_hidden_data_weak(name)
-# define libintl_hidden_data_ver(local, name)
-#endif
-
 #if defined NOT_IN_libc && defined IS_IN_libutil
 # define libutil_hidden_proto(name, attrs...) hidden_proto (name, ##attrs)
 # define libutil_hidden_def(name) hidden_def (name)

include/libintl.h
@@ -1,140 +1,0 @@
-/* Message catalogs for internationalization.
-   Copyright (C) 1995-2002, 2004, 2005 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
-   This file is derived from the file libgettext.h in the GNU gettext package.
-
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.  */
-
-#ifndef _LIBINTL_H
-#define _LIBINTL_H	1
-
-#include <features.h>
-
-#ifdef __UCLIBC_HAS_GETTEXT_AWARENESS__
-
-/* We define an additional symbol to signal that we use the GNU
-   implementation of gettext.  */
-#define __USE_GNU_GETTEXT 1
-
-/* Provide information about the supported file formats.  Returns the
-   maximum minor revision number supported for a given major revision.  */
-#define __GNU_GETTEXT_SUPPORTED_REVISION(major) \
-  ((major) == 0 ? 1 : -1)
-
-__BEGIN_DECLS
-
-/* Look up MSGID in the current default message catalog for the current
-   LC_MESSAGES locale.  If not found, returns MSGID itself (the default
-   text).  */
-extern char *gettext (const char *__msgid)
-     __THROW __attribute_format_arg__ (1);
-
-/* Look up MSGID in the DOMAINNAME message catalog for the current
-   LC_MESSAGES locale.  */
-extern char *dgettext (const char *__domainname, const char *__msgid)
-     __THROW __attribute_format_arg__ (2);
-#if 0 /* uClibc: disabled */
-extern char *__dgettext (const char *__domainname, const char *__msgid)
-     __THROW __attribute_format_arg__ (2);
-#endif
-
-/* Look up MSGID in the DOMAINNAME message catalog for the current CATEGORY
-   locale.  */
-extern char *dcgettext (const char *__domainname,
-			const char *__msgid, int __category)
-     __THROW __attribute_format_arg__ (2);
-#if 0 /* uClibc: disabled */
-extern char *__dcgettext (const char *__domainname,
-			  const char *__msgid, int __category)
-     __THROW __attribute_format_arg__ (2);
-#endif
-
-
-/* Similar to `gettext' but select the plural form corresponding to the
-   number N.  */
-extern char *ngettext (const char *__msgid1, const char *__msgid2,
-		       unsigned long int __n)
-     __THROW __attribute_format_arg__ (1) __attribute_format_arg__ (2);
-
-/* Similar to `dgettext' but select the plural form corresponding to the
-   number N.  */
-extern char *dngettext (const char *__domainname, const char *__msgid1,
-			const char *__msgid2, unsigned long int __n)
-     __THROW __attribute_format_arg__ (2) __attribute_format_arg__ (3);
-
-/* Similar to `dcgettext' but select the plural form corresponding to the
-   number N.  */
-extern char *dcngettext (const char *__domainname, const char *__msgid1,
-			 const char *__msgid2, unsigned long int __n,
-			 int __category)
-     __THROW __attribute_format_arg__ (2) __attribute_format_arg__ (3);
-
-
-/* Set the current default message catalog to DOMAINNAME.
-   If DOMAINNAME is null, return the current default.
-   If DOMAINNAME is "", reset to the default of "messages".  */
-extern char *textdomain (const char *__domainname) __THROW;
-
-/* Specify that the DOMAINNAME message catalog will be found
-   in DIRNAME rather than in the system locale data base.  */
-extern char *bindtextdomain (const char *__domainname,
-			     const char *__dirname) __THROW;
-
-/* Specify the character encoding in which the messages from the
-   DOMAINNAME message catalog will be returned.  */
-extern char *bind_textdomain_codeset (const char *__domainname,
-				      const char *__codeset) __THROW;
-
-
-/* Optimized version of the function above.  */
-#if defined __OPTIMIZE__ && !defined __cplusplus
-
-/* We need NULL for `gettext'.  */
-# define __need_NULL
-# include <stddef.h>
-
-/* We need LC_MESSAGES for `dgettext'.  */
-# include <locale.h>
-
-/* These must be macros.  Inlined functions are useless because the
-   `__builtin_constant_p' predicate in dcgettext would always return
-   false.  */
-
-# define gettext(msgid) dgettext (NULL, msgid)
-
-# define dgettext(domainname, msgid) \
-  dcgettext (domainname, msgid, LC_MESSAGES)
-
-# define ngettext(msgid1, msgid2, n) dngettext (NULL, msgid1, msgid2, n)
-
-# define dngettext(domainname, msgid1, msgid2, n) \
-  dcngettext (domainname, msgid1, msgid2, n, LC_MESSAGES)
-
-#endif	/* Optimizing.  */
-
-__END_DECLS
-
-#else
-
-#define gettext(msgid) ((const char *) (msgid))
-
-#endif /* __UCLIBC_HAS_GETTEXT_AWARENESS__ */
-
-#ifdef _LIBC
-# define _(x) gettext(x)
-# define N_(x) x
-#endif
-
-#endif /* libintl.h */

libc/Makefile.in
@@ -49,7 +49,6 @@ libc-a-$(UCLIBC_HAS_CRYPT) += $(libcrypt-a-y)
 libc-a-$(UCLIBC_HAS_ARGP) += $(libuargp-a-y)
 libc-a-$(UCLIBC_HAS_THREADS) += $(libpthread-a-y)
 libc-a-$(UCLIBC_HAS_REALTIME) += $(librt-a-y)
-libc-a-$(UCLIBC_HAS_GETTEXT_AWARENESS) += $(libintl-a-y)
 libc-a-$(UCLIBC_HAS_BACKTRACE) += $(libubacktrace-a-y)
 
 libc-so-y = $(libc-y:.o=.os) $(libc-shared-y)
@@ -61,7 +60,6 @@ libc-so-$(UCLIBC_HAS_CRYPT) += $(libcrypt-so-y)
 libc-so-$(UCLIBC_HAS_ARGP) += $(libuargp-so-y)
 libc-so-$(UCLIBC_HAS_THREADS) += $(libpthread-so-y)
 libc-so-$(UCLIBC_HAS_REALTIME) += $(librt-so-y)
-libc-so-$(UCLIBC_HAS_GETTEXT_AWARENESS) += $(libintl-so-y)
 libc-so-$(UCLIBC_HAS_BACKTRACE) += $(libubacktrace-so-y)
 
 lib-a-y += $(top_builddir)lib/libc.a

libc/inet/gai_strerror.c
@@ -18,7 +18,6 @@
 
 #include <stdio.h>
 #include <netdb.h>
-#include <libintl.h>
 
 static const struct
   {
@@ -27,22 +26,22 @@ static const struct
   }
 values[] =
   {
-    { EAI_ADDRFAMILY, N_("Address family for hostname not supported") },
-    { EAI_AGAIN, N_("Temporary failure in name resolution") },
-    { EAI_BADFLAGS, N_("Bad value for ai_flags") },
-    { EAI_FAIL, N_("Non-recoverable failure in name resolution") },
-    { EAI_FAMILY, N_("ai_family not supported") },
-    { EAI_MEMORY, N_("Memory allocation failure") },
-    { EAI_NODATA, N_("No address associated with hostname") },
-    { EAI_NONAME, N_("Name or service not known") },
-    { EAI_SERVICE, N_("Servname not supported for ai_socktype") },
-    { EAI_SOCKTYPE, N_("ai_socktype not supported") },
-    { EAI_SYSTEM, N_("System error") },
-    { EAI_INPROGRESS, N_("Processing request in progress") },
-    { EAI_CANCELED, N_("Request canceled") },
-    { EAI_NOTCANCELED, N_("Request not canceled") },
-    { EAI_ALLDONE, N_("All requests done") },
-    { EAI_INTR, N_("Interrupted by a signal") }
+    { EAI_ADDRFAMILY, "Address family for hostname not supported" },
+    { EAI_AGAIN, "Temporary failure in name resolution" },
+    { EAI_BADFLAGS, "Bad value for ai_flags" },
+    { EAI_FAIL, "Non-recoverable failure in name resolution" },
+    { EAI_FAMILY, "ai_family not supported" },
+    { EAI_MEMORY, "Memory allocation failure" },
+    { EAI_NODATA, "No address associated with hostname" },
+    { EAI_NONAME, "Name or service not known" },
+    { EAI_SERVICE, "Servname not supported for ai_socktype" },
+    { EAI_SOCKTYPE, "ai_socktype not supported" },
+    { EAI_SYSTEM, "System error" },
+    { EAI_INPROGRESS, "Processing request in progress" },
+    { EAI_CANCELED, "Request canceled" },
+    { EAI_NOTCANCELED, "Request not canceled" },
+    { EAI_ALLDONE, "All requests done" },
+    { EAI_INTR, "Interrupted by a signal" }
   };
 
 const char *
@@ -51,7 +50,7 @@ gai_strerror (int code)
     size_t i;
     for (i = 0; i < sizeof (values) / sizeof (values[0]); ++i)
 	if (values[i].code == code)
-	    return _(values[i].msg);
+	    return values[i].msg;
 
-    return _("Unknown error");
+    return "Unknown error";
 }

libc/inet/rpc/auth_unix.c
@@ -42,7 +42,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <libintl.h>
 #include <sys/param.h>
 
 #include <rpc/types.h>
@@ -105,7 +104,7 @@ authunix_create (char *machname, uid_t uid, gid_t gid, int len,
   if (auth == NULL || au == NULL)
     {
 no_memory:
-      (void) fputs (_("authunix_create: out of memory\n"), stderr);
+      (void) fputs ("authunix_create: out of memory\n", stderr);
       mem_free (auth, sizeof (*auth));
       mem_free (au, sizeof (*au));
       return NULL;
@@ -314,7 +313,7 @@ marshal_new_auth (AUTH *auth)
   xdrmem_create (xdrs, au->au_marshed, MAX_AUTH_BYTES, XDR_ENCODE);
   if ((!xdr_opaque_auth (xdrs, &(auth->ah_cred))) ||
       (!xdr_opaque_auth (xdrs, &(auth->ah_verf))))
-    perror (_("auth_unix.c - Fatal marshalling problem"));
+    perror ("auth_unix.c - Fatal marshalling problem");
   else
     au->au_mpos = XDR_GETPOS (xdrs);
 

commit 234ecfa4cd4e8e14d2dc0352ffde445409cdb509

getopt-susv3: remove call to gettext `_` macro
getopt-susv3.c is only compiled when UCLIBC_HAS_GNU_GETOPT is off (e.g.
allnoconfig) so when libintl support was removed this file was left behind

Fixes: 50aba30ae ("remove libintl stub and libintl.h header")
Signed-off-by: Charles Mirabile <cmirabil@redhat.com>

libc/unistd/getopt-susv3.c
@@ -93,7 +93,7 @@ int getopt(int argc, char * const argv[], const char *optstring)
 #ifdef __BCC__
 					fprintf(stderr, "%s: %s -- %c\n", argv[0], s, *o);
 #else
-					fprintf(stderr, _(s), argv[0], *o);
+					fprintf(stderr, s, argv[0], *o);
 #endif
 				}
 			}