Mu-L/uclibc-ng

commit ffd84a0f60f918a48e6e2f24e37e7c53291c0373

static pie: fix building static PDE
When uclibc is built with static PIE support the _dl_load_base variable
shared between the libc-tls.c and reloc_static_pie.c creates the
dependency that requires linking reloc_static_pie.o into static
position-dependent executables resulting in the following build errors:
  gcc -static test.c -o test
  ...ld:
  ...usr/lib/libc.a(reloc_static_pie.os):(.text+0x0):
	undefined reference to `_DYNAMIC'

Move _dl_load_base definition to libc-tls.c to resolve this dependency
and fix static PDE build.

Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

libc/misc/internals/reloc_static_pie.c
@@ -25,7 +25,7 @@
 #include <dl-startup.h>
 #endif
 
-ElfW(Addr) _dl_load_base = NULL;
+extern ElfW(Addr) _dl_load_base;
 
 void
 reloc_static_pie (ElfW(Addr) load_addr);

libpthread/nptl/sysdeps/generic/libc-tls.c
@@ -117,6 +117,10 @@ init_static_tls (size_t memsz, size_t align)
   GL(dl_tls_static_nelem) = GL(dl_tls_max_dtv_idx);
 }
 
+#if !defined(__FDPIC__) && !defined(SHARED) && defined(STATIC_PIE)
+ElfW(Addr) _dl_load_base;
+#endif
+
 void __libc_setup_tls (size_t tcbsize, size_t tcbalign);
 void
 __libc_setup_tls (size_t tcbsize, size_t tcbalign)
@@ -143,8 +147,7 @@ __libc_setup_tls (size_t tcbsize, size_t tcbalign)
 #else
 	  initimage = (void *) phdr->p_vaddr;
 #if !defined(SHARED) && defined(STATIC_PIE)
-    extern ElfW(Addr) _dl_load_base;
-    initimage += _dl_load_base;
+	  initimage += _dl_load_base;
 #endif
 #endif
 	  align = phdr->p_align;

commit 3b73b01e4dfe6fd95890f95040ad142cccc58de7

Fix build with static-pie but without nptl threads
since ffd84a0f6 the `_dl_load_base` variable is defined in the nptl code
and as such its existance is now gated behind UCLIBC_HAS_THREADS_NATIVE.
When building without that config (e.g. allnoconfig) an unresolved symbol
linking error occurs. Add a check to the static pie code reponsible for
setting that variable to only attempt to initialize it if nessecary.

Fixes: ffd84a0f6 ("static pie: fix building static PDE")
Signed-off-by: Charles Mirabile <cmirabil@redhat.com>

libc/misc/internals/reloc_static_pie.c
@@ -25,7 +25,9 @@
 #include <dl-startup.h>
 #endif
 
+#ifdef __UCLIBC_HAS_THREADS_NATIVE__
 extern ElfW(Addr) _dl_load_base;
+#endif
 
 void
 reloc_static_pie (ElfW(Addr) load_addr);
@@ -111,5 +113,7 @@ reloc_static_pie(ElfW(Addr) load_addr)
 			(void)rel_size;
 #endif
     }
+#ifdef __UCLIBC_HAS_THREADS_NATIVE__
     _dl_load_base = load_addr;
+#endif
 }